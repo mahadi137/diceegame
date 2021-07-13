9b51f3d3a010695fd75633b6d0449b7524cb1d08
Sayad Mohammad Mahadi Hassan <71854514+mahadi137@users.noreply.github.com>
2021-07-13 14:43:54 +0300

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,27 +1,31 @@
-//Player 1 Dicee Number Generation and Image Change
+function loadJS () {
+  
+  //Player 1 Dicee Number Generation and Image Change
 
-var player_1_DiceeNumber = Math.floor (Math.random () * 6) + 1;
+  var player_1_DiceeNumber = Math.floor (Math.random () * 6) + 1;
 
-var diceeImgNumber_1 = "images/dice" + player_1_DiceeNumber + ".png";
+  var diceeImgNumber_1 = "images/dice" + player_1_DiceeNumber + ".png";
 
-document.querySelector("img.dicee-img-1").setAttribute("src", diceeImgNumber_1);
+  document.querySelector("img.dicee-img-1").setAttribute("src", diceeImgNumber_1);
 
-//Player 2 Dicee Number Generation and Image Change
+  //Player 2 Dicee Number Generation and Image Change
 
-var player_2_DiceeNumber = Math.floor (Math.random () * 6) + 1;
+  var player_2_DiceeNumber = Math.floor (Math.random () * 6) + 1;
 
-var diceeImgNumber_2 = "images/dice" + player_2_DiceeNumber + ".png";
+  var diceeImgNumber_2 = "images/dice" + player_2_DiceeNumber + ".png";
 
-document.querySelector("img.dicee-img-2").setAttribute("src", diceeImgNumber_2);
+  document.querySelector("img.dicee-img-2").setAttribute("src", diceeImgNumber_2);
 
-//Change the header
+  //Change the header
 
-if (diceeImgNumber_1 == diceeImgNumber_2) {
-  document.querySelector("h1").innerHTML = "Draw!";
+  if (diceeImgNumber_1 == diceeImgNumber_2) {
+    document.querySelector("h1").innerHTML = "Draw!";
 
-} else if (diceeImgNumber_1 > diceeImgNumber_2) {
-  document.querySelector("h1").innerHTML = "Player 1 WINs!";
+  } else if (diceeImgNumber_1 > diceeImgNumber_2) {
+    document.querySelector("h1").innerHTML = "Player 1 WINs!";
 
-} else { //(diceeImgNumber_1 < diceeImgNumber_2)
-  document.querySelector("h1").innerHTML = "Player 2 WINs!";
+  } else { //(diceeImgNumber_1 < diceeImgNumber_2)
+    document.querySelector("h1").innerHTML = "Player 2 WINs!";
+  }
+  
 }
